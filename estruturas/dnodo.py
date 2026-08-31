65ebad37c4453cee6a7c0dabfe86d5a960322630
GUSTAVO DURANTE <1139541@atitus.local>
2026-08-31 20:53:47 -0300

    primeira venda

--- estruturas/dnodo.py
+++
@@ -1,5 +0,0 @@
-class DNodo:
-    def __init__(self, valor):
-        self.valor = valor
-        self.anterior = None
-        self.proximo = None
--- estruturas/nodo.py
+++
@@ -1,4 +0,0 @@
-class Nodo:
-    def __init__(self, valor):
-        self.valor = valor
-        self.proximo = None
